3858aae5f604c5a063d8e292a6fe870aba590f72
AeroRider <dave.butterworth.ee@gmail.com>
2024-09-11 15:36:37 -0500

    learning i/o code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [];
+var prompt = inquirer.createPromptModule();
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+prompt([{
+    type: 'input',
+    name: 'projectTitle',
+    message: 'enter your project title',
+}, {
+    type: 'input',
+    name: 'description',
+    message: 'enter your project description',
+}, {
+    type: 'input',
+    name: 'usage',
+    message: 'enter your project usage information',
+},
+    {
+        type: 'list',
+        name:  'license',
+        message: 'Select your project license',
+        choices: ['MIT', 'GNU2'],
+    }
+]).then(answers => {
+    const markdownTemplate = `## Description
+    `
+    // could add all the info into the template literal
 
-// TODO: Create a function to initialize app
-function init() {}
+    const description = ${answers.description};
 
-// Function call to initialize app
-init();
+    // repeat for other sections that will be at the TOC level
+
+    //console.log("answers: " + JSON.stringify(answers));
+    console.log(`${answers.projectTitle}` + JSON.stringify(answers));
+});
+
+generateMarkdown(prompt).then(answers => {})
+
+
+
+// this covered under xxxx license
+// it can be viewed at ./LICENSE
+// big thing is to have a question on picking one of 4 licences
+
+//GitHub username
+
+// email
+
+//TOC
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
+// put name of license itself int eh badge:
+// avoid spaces and dots.
+// Just use the link to set the name (MIT, ...)
+// render the badge
+
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -9,6 +14,9 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
+
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
